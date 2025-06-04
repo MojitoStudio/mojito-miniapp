387ee29f7af2f038c88208923b64360658b98f5d
MojitoStudio <alinamartinjaka@gmail.com>
2025-06-04 12:06:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+function openModal() {
+  document.getElementById('episodeModal').style.display = 'flex';
+}
+
+function closeModal() {
+  document.getElementById('episodeModal').style.display = 'none';
+}
+</script>
